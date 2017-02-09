5bce9b29c7b7a8bfa8fe234aed37b3525c626f86
DarkyZShadow <s.soulie@hotmail.fr>
2017-02-09 15:59:07 +0100

    Initial commit

---
+++ sources/common.h
@@ -0,0 +1,14 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+#include <stdio.h>
+
+#ifndef NULL
+#define NULL (void*)0
+#endif
+
+/* Libmy functions */
+void            my_putchar(char c);
+int             my_strlen(char *str);
+
+#endif
+
